3a88ca866aadca224a0e9194c96310a20e023037
Aniket Gavali <aniket.gavali@noovosoft.com>
2025-07-08 23:14:21 +0530

    New updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://aniket-dg.github.io",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
